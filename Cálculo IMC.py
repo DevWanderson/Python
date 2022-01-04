adfa69968421c273ff8d7f83b55ac3272adfbb3d
Wanderson Guedes <guedesw3@gmail.com>
2022-01-04 09:35:22 -0300

    main

--- Cálculo IMC.py
+++ Cálculo IMC.py
@@ -1,23 +1,38 @@
 #Cálculo IMC
 print("----------Cálculo IMC----------")
-print("-------------ALTURA------------")
+print("------------ALTURA-------------")
 
+#Altura deve ser inserida com ponto flutuante ex.:(1.75)
 altura = float(input("Informe a Altura: "))
 massa = float(input("Informe a Massa: "))
 #peso
 
-#altura = float(a)
-#assa = float(m)
+"""
+altura = float(a)
+assa = float(m)
+"""
 print(type(altura))
 
 imc = massa/(altura*altura)
 print(imc)
-if imc < 18.5: {
+
+if imc < 17 :
+    print("Você está MUITO abaixo do Peso")  
+
+elif (imc >= 17 and imc < 18.5):
     print("Você está abaixo do peso")
-}
-elif (imc >18.4 & imc <25): {
+
+elif(imc >= 18.5 and imc <25):
     print("Você está no Peso Ideal")
-}
-elif imc > 24.9 & <30{
-    print("Você está com sobrepeso")
-}
+
+elif(imc > 25 and imc <= 30):
+    print("Você está acima do peso")
+
+elif(imc > 30 and imc <=35):
+    print("Você está com Obesidade")
+    
+elif(imc > 35 and imc <= 40):
+    print("Você está com Obesidade Severa")
+
+else:
+    print("Você está com Obesidade Mórbida")
